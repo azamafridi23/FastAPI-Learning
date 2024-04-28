82fe685e6495b0ffe38ad53d1cb8feaa902b4807
Azam Afridi <azafridi9547@gmail.com>
2024-04-28 16:29:50 +0500

    Completed Project

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ AYNCHIO, AWAIT AND ASYNC IN PYTHON: https://www.youtube.com/watch?v=K56nNuBEd0c
 
 '''
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import User,Gender, Role
 from uuid import UUID,uuid4
 
@@ -36,4 +36,5 @@ async def delete_user(user_id:UUID):
         if user.id == user_id:
             db.remove(user)
             return ['deleted']
-    return ['not found']
+    return HTTPException(status_code=404, detail=f'user with id:{user_id} does not exist')
+
